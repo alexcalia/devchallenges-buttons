5b6e3128dc746aa4191677ed76417a18bec96fb9
Alex Calia <alex.m.calia@outlook.com>
2021-02-16 10:41:15 -0500

    restructured theme file, adding sizes

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,30 +13,31 @@ const StyledButton = styled.button`
   font-family: Noto Sans JP;
   border-radius: 6px;
   padding: 10px;
-  box-shadow: ${props => props.disableShadow || props.variant ? 'none' : `0px 2px 3px ${props.theme[props.color].shadowColor}`};
-  background: ${props => props.variant ? '#FFF' : props.theme[props.color].bg.main};
+  box-shadow: ${props => props.disableShadow || props.variant ? 'none' : `0px 2px 3px ${props.theme.colors[props.color].shadowColor}`};
+  background: ${props => props.variant ? '#FFF' : props.theme.colors[props.color].bg.main};
 
   &:hover {
     background-color: ${props => props.disabled 
-      ? props.theme[props.color].bg.main 
+      ? props.theme.colors[props.color].bg.main 
       : props.variant 
       ? 'rgba(41, 98, 255, 0.1)' 
-      :props.theme[props.color].bg.hover}
+      :props.theme.colors[props.color].bg.hover}
   };
 
   &:focus {
     background-color: ${props => props.disabled 
-      ? props.theme[props.color].bg.main 
+      ? props.theme.colors[props.color].bg.main 
       : props.variant 
       ? 'rgba(41, 98, 255, 0.1)' 
-      :props.theme[props.color].bg.hover}
+      :props.theme.colors[props.color].bg.hover}
   };
   
   color: ${props => props.disabled 
             ? '#9E9E9E' 
             : props.variant
             ? '#3D5AFE'
-            :props.theme[props.color].color};
+            :props.theme.colors[props.color].color
+          };
 `
 
 
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,36 +1,51 @@
  const theme = {
-  default: {
-    bg: {
-      main: '#E0E0E0',
-      hover: '#AEAEAE',
+  colors: {
+    default: {
+      bg: {
+        main: '#E0E0E0',
+        hover: '#AEAEAE',
+      },
+      color: '#3F3F3F',
+      shadowColor: 'rgba(51, 51, 51, 0.2)'
     },
-    color: '#3F3F3F',
-    shadowColor: 'rgba(51, 51, 51, 0.2)'
+    primary: {
+      bg: {
+        main: '#2962FF',
+        hover: '#0039CB'
+      },
+      color: '#FFF',
+      shadowColor: 'rgba(41, 98, 255, 0.2)'
+    },
+    secondary: {
+      bg: {
+        main: '#455A64',
+        hover: '#1C313A'
+      },
+      color: '#FFF',
+      shadowColor: 'rgba(69, 90, 100, 0.2)'
+    },
+    danger: {
+      bg: {
+        main: '#D32F2F',
+        hover: '#9A0007'
+      },
+      color: '#FFF',
+      shadowColor: 'rgba(211, 47, 47, 0.2'
+    }
   },
-  primary: {
-    bg: {
-      main: '#2962FF',
-      hover: '#0039CB'
+
+  size: {
+    sm: {
+
     },
-    color: '#FFF',
-    shadowColor: 'rgba(41, 98, 255, 0.2)'
-  },
-  secondary: {
-    bg: {
-      main: '#455A64',
-      hover: '#1C313A'
+    md: {
+
     },
-    color: '#FFF',
-    shadowColor: 'rgba(69, 90, 100, 0.2)'
-  },
-  danger: {
-    bg: {
-      main: '#D32F2F',
-      hover: '#9A0007'
-    },
-    color: '#FFF',
-    shadowColor: 'rgba(211, 47, 47, 0.2'
+    lg: {
+      
+    }
   }
+  
 };
 
 export default theme;
